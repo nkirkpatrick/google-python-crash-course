15e7c1e892cbe962b1b229487b550bbe5a46ad49
Norbert Kirkpatrick <norbertkirkpatrick@norberts-mbp.lan>
2020-05-17 17:54:00 -0700

    List examples

---
+++ lists/list_examples.py
@@ -0,0 +1,19 @@
+x = ["Now", "we", "are", "cooking!"]
+
+print(type(x))
+
+print(x)
+
+print("are" in x)
+
+print("Today" in x)
+
+print(x[0])
+
+print(x[3])
+
+print(x[1:3])
+
+print(x[:2])
+
+print(x[2:])
