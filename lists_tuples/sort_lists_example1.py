90ac63cd91bf5be5e22e41efc7e7b89b8afe142b
Norbert Kirkpatrick <norbertkirkpatrick@macbook-pro.lan>
2020-07-04 17:25:37 -0700

    user report notebook update

---
+++ lists_tuples/sort_lists_example1.py
@@ -0,0 +1,15 @@
+numbers = [4, 6, 2, 7, 1]
+
+numbers.sort()
+print(numbers)
+
+names = ["Carlos", "Ray", "Alex", "Kelly"]
+
+print(names)
+
+print(sorted(names))
+
+print(names)
+
+print(sorted(names, key=len))
+
